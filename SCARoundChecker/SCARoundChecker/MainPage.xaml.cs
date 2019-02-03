295f0c32c4a58a930f29d1f7bb2f48de12c9ccd8
Benji Altman <benji@altmansoftwaredesign.com>
2019-02-02 23:19:09 -0600

    Got http requset working

--- SCARoundChecker/SCARoundChecker/MainPage.xaml.cs
+++ SCARoundChecker/SCARoundChecker/MainPage.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
@@ -11,15 +13,37 @@ namespace SCARoundChecker
 	{
 		public MainPage()
 		{
-			DateTime now = DateTime.Now;
+			//DateTime now = DateTime.Now;
+			DateTime now = new DateTime(2019, 2, 2);
 			int season = now.Year;
 			if (now.Month <= 6)
 				season--;
 			string link0 = $"http://www.schoolchess.org/old/results/ChooseTour.php?season={season}-{season+1}&tourDate={now.ToString("yyyy-MM-dd")}";
-			string link1 = "http://www.schoolchess.org/old/results/ChooseTour.php?season=2018-2019&tourDate=2019-02-02";
+
+
+			// Create a request for the URL. 		
+			WebRequest request = WebRequest.Create(link0);
+			// If required by the server, set the credentials.
+			request.Credentials = CredentialCache.DefaultCredentials;
+			// Get the response.
+			HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+			// Display the status.
+			Console.WriteLine(response.StatusDescription);
+			// Get the stream containing content returned by the server.
+			Stream dataStream = response.GetResponseStream();
+			// Open the stream using a StreamReader for easy access.
+			StreamReader reader = new StreamReader(dataStream);
+			// Read the content.
+			string responseFromServer = reader.ReadToEnd();
+			// Cleanup the streams and the response.
+			reader.Close();
+			dataStream.Close();
+			response.Close();
+
+
 			InitializeComponent();
 			label.Text = link0;
-			webView.Source = link0;
+			stupid.Text = responseFromServer;
 		}
 	}
 }
