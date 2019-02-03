44d5213d114178869f0331d5e14a44842cc7273d
Benji Altman <benji@altmansoftwaredesign.com>
2019-02-02 22:33:43 -0600

    Webview can work on android

--- SCARoundChecker/SCARoundChecker/MainPage.xaml.cs
+++ SCARoundChecker/SCARoundChecker/MainPage.xaml.cs
@@ -11,7 +11,15 @@ namespace SCARoundChecker
 	{
 		public MainPage()
 		{
+			DateTime now = DateTime.Now;
+			int season = now.Year;
+			if (now.Month <= 6)
+				season--;
+			string link0 = $"http://www.schoolchess.org/old/results/ChooseTour.php?season={season}-{season+1}&tourDate={now.ToString("yyyy-MM-dd")}";
+			string link1 = "http://www.schoolchess.org/old/results/ChooseTour.php?season=2018-2019&tourDate=2019-02-02";
 			InitializeComponent();
+			label.Text = link0;
+			webView.Source = link0;
 		}
 	}
 }
